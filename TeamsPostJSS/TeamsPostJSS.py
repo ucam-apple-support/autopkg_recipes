8b71f217756a16834a3a3142e292d21bd64e4553
James Nairn <jwrn3@cam.ac.uk>
2020-04-03 13:11:35 +0100

    Ensured compatibility with AutoPkg v1.x

--- TeamsPostJSS/TeamsPostJSS.py
+++ TeamsPostJSS/TeamsPostJSS.py
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import
+from __future__ import print_function
+
 import requests
 
 from autopkglib import Processor, ProcessorError
